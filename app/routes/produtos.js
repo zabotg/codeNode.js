d441b562b87240f3328c6ea9fa890b161a063c84
Guilherme Zabot <zabot.gui@gmail.com>
2017-05-02 18:13:11 -0300

    Respeitando o Accept

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -17,18 +17,32 @@ module.exports = function(app) {
    });
 
    app.get("/produtos/form", function(request, response){
-      response.render("produtos/form");
+      response.render("produtos/form", {errosValidacao:{}, produto:{}});
    });
 
    app.post("/produtos",function(request, response){
 
       var produto = request.body;
-      console.log(produto);
+      request.assert("titulo", "Titulo é obrigatório").notEmpty();
+      request.assert("preco", "Formato inválido").isFloat();
+
+      var erros = request.validationErrors();
+
+      if(erros){
+         response.format({
+            html: function(){
+               response.status(400).render("produtos/form",{errosValidacao:erros, produto:produto});
+            },
+            json: function(){
+               response.status(400).json(erros);
+            }
+         });
+         return;
+      }
 
        var connection = app.infra.connectionFactory();
        var produtosDAO = new app.infra.ProdutosDAO(connection);
        produtosDAO.salva(produto, function(erros, resultados){
-          console.log(erros);
           response.redirect("/produtos");
       });
    });
--- cadastra-livros-terminal.js
+++ cadastra-livros-terminal.js
@@ -19,7 +19,7 @@ var client = http.request(configuracoes, function(response){
 });
 
 var produto = {
-   titulo: "mais sobre node",
+   titulo: "",
    descricao: "node, javascript e um pouco sobre http",
    preco: 100
 };
